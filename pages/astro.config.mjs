3a8ba09ce14bbf1e64f9e8d62faa3f59ef697b42
Greg Ball <greg@baller.software>
2026-03-12 23:22:48 -0500

    rectivied path issue

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gballer77.github.io',
-  base: '/gspec',
+  base: '/gspec/',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
