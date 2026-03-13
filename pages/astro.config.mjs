bb30063613b4cbb1b774fc1a6110ef65f175705c
Greg Ball <greg@baller.software>
2026-03-13 11:51:01 -0500

    Removed base path

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gballer77.github.io',
-  base: '/gspec/',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
